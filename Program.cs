76ac8f397207502e147fe8e5557d7f9d97d7057a
shivam <shivam@DESKTOP-BT1O840>
2023-08-25 22:06:17 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+static class MathHelper
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Array cannot be empty or null.");
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numberArray = { 10, 20, 30, 40, 50 };
+
+        double average = MathHelper.CalculateAverage(numberArray);
+
+        Console.WriteLine($"Average: {average:F2}");
+    }
+}
